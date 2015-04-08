ae1904dd7ed67e1607d666726afb475dcea8ffb9
Sean Oldham <seanosaur@me.com>
2015-04-08 08:40:20 -0500

    click out of popover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,13 @@ $(document).ready(function() {
     html: true
   });
 });
+
+$('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
