32600751e920dd72218f861a4ec2aff61b1c6f9c
Sean Oldham <seanosaur@me.com>
2015-04-11 21:38:54 -0500

    update time zone

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,12 @@ module Mlb
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+
     # Be sure to have the adapter's gem in your Gemfile and follow
     # the adapter's specific installation and deployment instructions.
     # config.active_job.queue_adapter = :sidekiq
